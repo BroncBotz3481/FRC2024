7c304dca35932d48df48250ef42f5fa8c501aa7e
Mihir Ghawghawe <Mihirg.301@gmail.com>
2024-01-19 11:42:34 -0600

    Add TODO for driveConversionFactor update
    A comment has been added to remind of a necessary update to the driveConversionFactor. The DriveGear ratio in the driveConversionFactor should be adjusted to 6.12, to reflect the L3 variant.

--- src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
@@ -58,6 +58,7 @@ public class SwerveSubsystem extends SubsystemBase
     //  In this case the wheel diameter is 4 inches, which must be converted to meters to get meters/second.
     //  The gear ratio is 6.75 motor revolutions per wheel rotation.
     //  The encoder resolution per motor revolution is 1 per motor revolution.
+    //TODO Change driveConversionFactor DriveGear ratio to 6.12 which is the L3 variant
     double driveConversionFactor = SwerveMath.calculateMetersPerRotation(Units.inchesToMeters(4), 6.75, 1);
     System.out.println("\"conversionFactor\": {");
     System.out.println("\t\"angle\": " + angleConversionFactor + ",");
