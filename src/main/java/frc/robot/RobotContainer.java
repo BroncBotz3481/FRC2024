ee423a419d18bd1c1194e4c0d2e8c4613e4a8b91
Mihir Ghawghawe <Mihirg.301@gmail.com>
2024-02-03 01:45:32 -0600

    Update autonomous command and tweak PID constants
    Several adjustments were made to improve the robot's functionality. The autonomous command was updated to use a new path planner and to add a simple and complex auto. Additionally, tweaks were made to PID constants and controller properties to refine the robot's movements. The YAGSL version was updated for better swerve library compatibility.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.commands.PathPlannerAuto;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -11,6 +13,8 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
@@ -33,6 +37,9 @@ public class RobotContainer
   // The robot's subsystems and commands are defined here...
   private final SwerveSubsystem drivebase = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(),
                                                                          "swerve/neo"));
+
+//  private final SendableChooser<Command> autoChooser;
+
   // CommandJoystick rotationController = new CommandJoystick(1);
   // Replace with CommandPS4Controller or CommandJoystick if needed
   // CommandJoystick driverController = new CommandJoystick(1);
@@ -66,6 +73,14 @@ public class RobotContainer
 
     drivebase.setDefaultCommand(
         !RobotBase.isSimulation() ? driveFieldOrientedDirectAngle : driveFieldOrientedDirectAngle);
+
+    // Build an auto chooser. This will use Commands.none() as the default option.
+//    autoChooser = AutoBuilder.buildAutoChooser();
+
+    // (OPTIONAL) Another option that allows you to specify the default auto by its name
+    // autoChooser = AutoBuilder.buildAutoChooser("My Default Auto");
+
+//    SmartDashboard.putData("Auto Chooser", autoChooser);
   }
 
   /**
@@ -97,8 +112,14 @@ public class RobotContainer
    */
   public Command getAutonomousCommand()
   {
-    // An example command will be run in autonomous
-    return drivebase.getAutonomousCommand("TESTER", true);
+    // A Path will be run in autonomous
+    // return drivebase.getAutonomousCommand("TESTER", true);
+
+    // Runs an Auto
+    return new PathPlannerAuto("Simple Auto");
+
+    // Gets Selected Auto from Shuffleboard
+    // return autoChooser.getSelected();
   }
 
   public void setDriveMode()
