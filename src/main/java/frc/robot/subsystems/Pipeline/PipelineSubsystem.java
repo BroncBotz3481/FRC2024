dffd0eebabfd295bb3ddc17076ced171980572cf
Orry Huang <orryhuang@gmail.com>
2024-02-13 18:01:24 -0600

    Create Pipeline and constants for Pipeline

--- src/main/java/frc/robot/subsystems/Pipeline/PipelineSubsystem.java
+++ src/main/java/frc/robot/subsystems/Pipeline/PipelineSubsystem.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems.Pipeline;
 
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import org.photonvision.PhotonCamera;
 
 public class PipelineSubsystem extends SubsystemBase {
     public PipelineSubsystem() {
@@ -9,6 +10,7 @@ public class PipelineSubsystem extends SubsystemBase {
         //       in the constructor or in the robot coordination class, such as RobotContainer.
         //       Also, you can call addChild(name, sendableChild) to associate sendables with the subsystem
         //       such as SpeedControllers, Encoders, DigitalInputs, etc.
+        PhotonCamera camera = new PhotonCamera("photonvision");
     }
 }
 
